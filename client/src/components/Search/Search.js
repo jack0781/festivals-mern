03267316515db380c6cea80730f1eee1b5161c6e
Praful Patel <praful@techalchemy.co>
2020-11-09 12:15:42 +0530

    name correct

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,31 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
-import FileBase from 'react-file-base64';
 import SearchBar from "material-ui-search-bar";
-
 import useStyles from './styles';
-import { createPost } from '../../actions/posts';
+import ReactDOM from "react-dom";
+import { Link, useLocation, BrowserRouter as Router } from "react-router-dom";
 
 const Search = ({ currentId, setCurrentId }) => {
-  const [postData, setPostData] = useState({ search: ''});
+  const [cardData, setCardData] = useState({ search: ''});
   const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
   const dispatch = useDispatch();
   const classes = useStyles();
   useEffect(() => {
-    if (post) setPostData(post);
+    if (post) setCardData(post);
   }, [post]);
   const handleChange = e => this.setState({ [e.target.name]: e.target.value });
   const clear = () => {
     setCurrentId(0);
-    setPostData({ search: ''});
+    setCardData({ search: ''});
   };
 
   return (
     <Paper className={classes.paper}>
      
       <form autoComplete="on" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleChange}>
-      <SearchBar name="search" fullWidth value={postData.searchresult} onChange={this.handleChange}
+      <SearchBar name="search" fullWidth value={cardData.searchresult} onChange={this.handleChange}
      style={{
        margin: '0 auto',
        maxWidth: 400
